47e4ce24b5e1a5afa20fce229a71391299fa25a9
burtonjoshuatoone <burtonjoshuatoone@gmail.com>
2026-02-03 19:41:59 -0700

    Jest Testing, Added rating to product

--- Index.js
+++ Index.js
@@ -1,11 +1,10 @@
 const getResponse = await fetch("http://localhost:5202/products");
 const productsJson = await getResponse.json();
-console.log(productsJson);
 const products_list = document.querySelector("ul[name=products_list]");
 for (let i = 0; i < productsJson.length; i++) {
   const product = productsJson[i];
   const { inventory_count, price } = product;
   const newLi = document.createElement("li");
-  newLi.innerText = `Price: ${price}, Inventory Count: ${inventory_count}`;
+  newLi.innerText = `Price: ${price}, Inventory Count: ${inventory_count}, Rating: ${rating}`;
   products_list.appendChild(newLi);
 }
